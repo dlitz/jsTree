10254d63cfc002951d93a2920678d90a9ff0ad26
ivan.bozhanov <ivan.bozhanov@7ee4f3fb-cb41-0410-822c-dd8830bd4fbc>
2008-10-07 12:09:02 +0000

    git-svn-id: http://jstree.googlecode.com/svn/trunk@39 7ee4f3fb-cb41-0410-822c-dd8830bd4fbc

--- jsTree/tree_component.js
+++ jsTree/tree_component.js
@@ -7,7 +7,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Date: 2008-10-06
+ * Date: 2008-10-07
  *
  */
 function tree_component () {
